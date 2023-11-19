3898fb31e9f21652e15695945dab3c6b1d511872
Elvis Rivera <therealelvisrivera@gmail.com>
2023-11-18 20:41:29 -0600

    new commits

--- froyo.js
+++ froyo.js
@@ -5,18 +5,22 @@ const userInput = prompt("Enter your favorite froyo flavors,Seperate with a comm
 //Parsing user input into Flavor Array
 
 
-
+//place holder for flavor array
 const observedFlavors = {};
 
 function userFlavors(userInput) {
 
     const flavorsArray = userInput.split(",");
-
+//for loop to add to array
     for (let i = 0; i < flavorsArray.length; i++) {
         const flavor = flavorsArray[i];
         observedFlavors[flavor] = true;
     }
 }
+
+
+//print user input into flavor array
+userFlavors(userInput);
 
 console.log(observedFlavors);
     
